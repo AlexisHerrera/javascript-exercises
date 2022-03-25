0bdde059e8046d100e15744d5ae0375a18946a55
Alexis Herrera <alexysaguilar@gmail.com>
2022-03-25 15:44:41 -0300

    Finish exercises from 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let args = Array.prototype.slice.call(arguments, 1);
+    for (const element of args) {
+        let index = array.indexOf(element);
+        if (index !== -1) {
+            array.splice(index,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
